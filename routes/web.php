d39cfbf62846bf2249fa1ae6070c77f1115859dc
sergio.cruz@outlook.pt <sergio.cruz@outlook.pt>
2025-09-07 22:23:29 +0100

    Procfile for Heroku

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PlayersController;
 
-Route::get('login', [AuthController::class, 'showLogin'])->name('login');
-Route::post('login', [AuthController::class, 'login']);
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
 
 
-Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-Route::get('dashboard', [PlayersController::class, 'listPlayer'])->middleware('auth');
+Route::get('/dashboard', [PlayersController::class, 'listPlayer'])->middleware('auth');
 
 Route::get('/',[AuthController::class, 'showLogin'])->name('login');
